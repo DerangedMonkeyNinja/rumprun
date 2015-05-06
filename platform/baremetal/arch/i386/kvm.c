2bc9da80fe29a8cb5ee4be93ec58e8e6433071ad
Timmons C. Player <timmons.player@spirent.com>
2015-05-06 14:16:39 -0400

    Use the correct KVM clock MSR based on the cpuid info

--- platform/baremetal/arch/i386/kvm.c
+++ platform/baremetal/arch/i386/kvm.c
@@ -10,6 +10,10 @@
 static struct pvclock_vcpu_time_info hv_clock;
 static struct pvclock_wall_clock wall_clock;
 
+/* when we use these MSR's */
+static uint32_t kvm_msr_system_time;
+static uint32_t kvm_msr_wall_clock;
+
 static uint64_t kvm_get_timecount(struct timecounter *tc);
 
 static struct timecounter kvm_timecounter = {
@@ -37,19 +41,26 @@ int has_kvm_clock(void)
 	u_int regs[4];
 
 	cpuid(0x40000001, regs);
-	if (regs[0] & KVM_FEATURE_CLOCKSOURCE2)
+	if (regs[0] & KVM_FEATURE_CLOCKSOURCE2) {
+		kvm_msr_system_time = KVM_MSR_SYSTEM_TIME_NEW;
+		kvm_msr_wall_clock = KVM_MSR_WALL_CLOCK_NEW;
 		return (1);
-	else
-		return (0);
+	} else if (regs[0] && KVM_FEATURE_CLOCKSOURCE) {
+		kvm_msr_system_time = KVM_MSR_SYSTEM_TIME_OLD;
+		kvm_msr_wall_clock = KVM_MSR_WALL_CLOCK_OLD;
+		return (1);
+	} else {
+ 		return (0);
+	}
 }
 
 static
 void init_kvm_pvclock_tc(void)
 {
 	/* XXX: see if wall clock works */
-	wrmsr(KVM_MSR_WALL_CLOCK_NEW, ((uintptr_t)&wall_clock));
+	wrmsr(kvm_msr_wall_clock, ((uintptr_t)&wall_clock));
 
-	wrmsr(KVM_MSR_SYSTEM_TIME_NEW, ((uintptr_t)&hv_clock | 1));
+	wrmsr(kvm_msr_system_time, ((uintptr_t)&hv_clock | 1));
 	bmk_tc_init(&kvm_timecounter);
 }
 
